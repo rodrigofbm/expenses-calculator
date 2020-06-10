ef75586b1b3b63ceb646ecea505d7ada0ae95acc
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2020-06-10 16:49:03 -0300

    reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,13 @@
   import Navbar from './Navbar.svelte';
   import Title from './Title.svelte';
   import ExpensesList from './ExepensesList.svelte';
+  import Totals from './Totals.svelte';
   // data
   import expensesData from './expenses.js';
   // variables
   let expenses = [...expensesData];
+  // reactive
+  $: total = expenses.reduce((acc, curr) => acc += curr.amount, 0);
   // functions
   function removeExpense(id) {
     expenses = expenses.filter(item => item.id !== id);
@@ -21,6 +24,7 @@
 
 <Navbar />
 <main class="content">
+  <Totals title="total expenses" {total} />
   <ExpensesList {expenses} />
   <button 
     type="button" 
