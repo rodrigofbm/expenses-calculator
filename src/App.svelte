c23b207ca37b38b75e8ace94060caab35136f0bc
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2020-06-11 17:06:25 -0300

    edit expanse and show/hide form

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,96 @@
 <script>
-  import { setContext } from 'svelte';
+  import { setContext } from "svelte";
   // components
-  import Navbar from './Navbar.svelte';
-  import Title from './Title.svelte';
-  import ExpensesList from './ExepensesList.svelte';
-  import Totals from './Totals.svelte';
-  import ExpenseForm from './ExpenseForm.svelte';
+  import Navbar from "./Navbar.svelte";
+  import Title from "./Title.svelte";
+  import ExpensesList from "./ExepensesList.svelte";
+  import Totals from "./Totals.svelte";
+  import ExpenseForm from "./ExpenseForm.svelte";
   // data
-  import expensesData from './expenses.js';
+  import expensesData from "./expenses.js";
   // variables
   let expenses = [...expensesData];
+  let setName = "";
+  let setAmount = null;
+  let setId = null;
+  // toggle form variables
+  let isFormOpen = false;
   // reactive
-  $: total = expenses.reduce((acc, curr) => acc += curr.amount, 0);
+  $: total = expenses.reduce((acc, curr) => (acc += curr.amount), 0);
+  $: isEditing = !!setId;
   // functions
+  function showForm() {
+    isFormOpen = true;
+  }
+
+  function hideForm() {
+    isFormOpen = false;
+
+    setId = null;
+    setAmount = null;
+    setName = "";
+  }
+
+  function addExpense({ name, amount }) {
+    amount = parseFloat(amount);
+    const expense = {
+      id: Math.random() * Date.now(),
+      name,
+      amount
+    };
+    expenses = [expense, ...expenses];
+  }
+
+  function setModifiedExpense(expenseId) {
+    const expense = expenses.find(item => item.id === expenseId);
+
+    setId = expense.id;
+    setName = expense.name;
+    setAmount = expense.amount;
+
+    showForm();
+  }
+
+  function editExpense({ name, amount }) {
+    amount = parseFloat(amount);
+    expenses = expenses.map(exp =>
+      exp.id === setId ? { ...exp, name, amount } : { ...exp }
+    );
+
+    setId = null;
+    setAmount = null;
+    setName = "";
+  }
+
   function removeExpense(id) {
     expenses = expenses.filter(item => item.id !== id);
   }
+
   function clearExpenses() {
     expenses = [];
   }
   // context
   setContext("remove", removeExpense);
+  setContext("modify", setModifiedExpense);
 </script>
 
-<Navbar />
+<Navbar {showForm} />
 <main class="content">
-  <ExpenseForm />
+  {#if isFormOpen}
+    <ExpenseForm
+      {addExpense}
+      {editExpense}
+      {isEditing}
+      {hideForm}
+      name={setName}
+      amount={setAmount} />
+  {/if}
   <Totals title="total expenses" {total} />
   <ExpensesList {expenses} />
-  <button 
-    type="button" 
+  <button
+    type="button"
     class="btn btn-primary btn-block"
-    on:click={clearExpenses}
-  >
+    on:click={clearExpenses}>
     clear expenses
   </button>
 </main>
--- src/ExpenseForm.svelte
+++ src/ExpenseForm.svelte
@@ -1,28 +1,54 @@
 <script>
-  import Title from './Title.svelte';
+  import Title from "./Title.svelte";
+
+  export let name = "";
+  export let amount = null;
+  export let hideForm;
+  export let addExpense;
+  export let editExpense;
+  export let isEditing;
+
+  $: isEmpty = !name || !amount;
+
+  function handleSubmit() {
+    if (isEditing) {
+      editExpense({ name, amount });
+    } else {
+      addExpense({ name, amount });
+    }
+
+    name = "";
+    amount = null;
+  }
 </script>
 
 <section class="form">
   <Title title="add expense" />
 
-  <form class="expense-form">
+  <form class="expense-form" on:submit|preventDefault={handleSubmit}>
     <div class="form-control">
       <label for="name">name</label>
-      <input type="text" id="name">
+      <input type="text" id="name" bind:value={name} />
     </div>
 
     <div class="form-control">
       <label for="amount">amount</label>
-      <input type="text" id="amount">
+      <input type="text" id="amount" bind:value={amount} />
     </div>
 
-    <p class="form-empty">Please fill out all form fields</p>
-  
-    <button type="submit" class="btn btn-block">
-      add expense
+    {#if isEmpty}
+      <p class="form-empty">Please fill out all form fields</p>
+    {/if}
+
+    <button
+      type="submit"
+      class="btn btn-block"
+      class:disabled={isEmpty}
+      disabled={isEmpty}>
+      {isEditing ? 'edit expense' : 'add expense'}
     </button>
 
-    <button type="button" class="close-btn">
+    <button type="button" class="close-btn" on:click={() => hideForm()}>
       <i class="fas fa-times" />
       close
     </button>
