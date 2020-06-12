52af2de8dfe35bb0e475aee85d56d6e0a250648f
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2020-06-12 00:23:39 -0300

    change the site's name

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -4,7 +4,7 @@
 
 <nav class="nav">
   <div class="nav-center">
-    <h1 class="nav-title">Budget Calculator</h1>
+    <h1 class="nav-title">Expenses Calculator</h1>
     <button type="button" class="nav-btn" on:click={() => showForm()}>
       <i class="far fa-plus-square">add item</i>
     </button>
